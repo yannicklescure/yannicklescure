39497ce3aadeca301fcf6320ba7ed0730ecb8e57
yannick lescure <yannick.lescure@gmail.com>
2020-09-25 12:27:29 -0400

    Init vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,48 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+environment.splitChunks((config) => Object.assign({}, config, {
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      // minRemainingSize: 0,
+      maxSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
+}))
+
+environment.splitChunks()
+
 module.exports = environment
